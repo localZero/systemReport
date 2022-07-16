b5cee05fa4c9ae1a9413b0838d9a8aede89ca415
zero <61998674+localZero@users.noreply.github.com>
2022-07-16 15:23:32 -0300

    Update fxmanifest.lua
    só coloquei o meu Discord.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,6 @@ game "gta5"
 
 server_scripts {"@vrp/lib/utils.lua", "sv.lua"}
 
-author 'Zero'
+author 'Zero / https://discord.gg/2uUaUhkt'
 description 'Esse script foi feito para auxilar nas denuncias, dentro do game.'
 version '1.0'
